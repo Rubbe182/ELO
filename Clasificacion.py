dbaf9ccc2dffd1ae77c1ff502a2821081c8c171b
Rubén Martínez Talayero <31803926+Rubbe182@users.noreply.github.com>
2023-09-21 17:43:31 +0200

    Update Clasificacion.py

--- Clasificacion.py
+++ Clasificacion.py
@@ -8,11 +8,11 @@ tu_dataframe = pd.read_csv('Classification.csv')
 # Título de la aplicación
 st.title('Filtrar DataFrame por Liga')
 
-# Sidebar para la selección múltiple
-ligas_seleccionadas = st.sidebar.multiselect('Selecciona Ligas', tu_dataframe['League'].unique())
+ligas_seleccionadas = st.multiselect('Selecciona Ligas', tu_dataframe['League'].unique())
 
 # Filtra el DataFrame basado en las ligas seleccionadas
 df_filtrado = tu_dataframe[tu_dataframe['League'].isin(ligas_seleccionadas)]
+
 
 # Muestra el DataFrame filtrado
 st.write('DataFrame Filtrado:', df_filtrado)
